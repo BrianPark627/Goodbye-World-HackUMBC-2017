5ddcb5b24646249b82c24c915902302d38c39476
Vishal Patel <patelvishal3199@gmail.com>
2017-10-08 13:10:48 -0400

    final submission

--- src/goodbye/GUI.java
+++ src/goodbye/GUI.java
@@ -2,6 +2,7 @@ package goodbye;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -15,43 +16,50 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class GUI extends Application {
 	private final static String EDIT_DELIMITER = "*edit*";
 	
+	private final static String BUG_TEMPLATE = "bugs/bugINDEX.txt";
+	private final static String FIX_TEMPLATE = "fixes/fixINDEX.txt";
+	private final static String ERR_TEMPLATE = "errs/errINDEX.txt";
+	private final static String MSG_TEMPLATE = "msgs/msgINDEX.txt";
+	
 	private final static int WIDTH = 700;
 	private final static int HEIGHT = 300;
 	
-    @Override
-    public void start(Stage stage) throws Exception {
-    	// desktop image stuff
-    	File desktopFile = new File("INSERT URL HERE");
-    	if (!desktopFile.exists()) {
-    		// throw new FileNotFoundException(desktopFile.toString());
+	ListedLabelBox errorBox = new ListedLabelBox(this);
+	ListedLabelBox codeBox = new ListedLabelBox(this, Color.BEIGE, Color.BLACK);
+	private int index = 1;
+	private final static int MAX_INDEX = 10;
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		// desktop image stuff
+		File desktopFile = new File("INSERT URL HERE");
+		if (!desktopFile.exists()) {
+			// throw new FileNotFoundException(desktopFile.toString());
 		}
-    	String desktopURL = desktopFile.toURI().toURL().toString();
-    	System.out.println(desktopURL);
-    	Image desktopImg = new Image(desktopURL);
+		String desktopURL = desktopFile.toURI().toURL().toString();
+		System.out.println(desktopURL);
+		Image desktopImg = new Image(desktopURL);
 		ImageView desktop = new ImageView(desktopImg);
 		
 		// root stuff
 		StackPane root = new StackPane();
-		root.setAlignment(Pos.TOP_LEFT);
+		root.setAlignment(Pos.CENTER);
 		
-		// error box
-		ListedLabelBox errorBox = new ListedLabelBox();
-		readControls(errorBox, "errs/err1.txt", EDIT_DELIMITER);
-		
-		// code editor box
-		ListedLabelBox codeBox = new ListedLabelBox(Color.BEIGE, Color.BLACK);
-		readControls(codeBox, "bugs/bug1.txt", EDIT_DELIMITER);
+		this.loadSet();
 	
 		HBox boxes = new HBox();
-		VBox eb = errorBox.getBox();
-		VBox cb = codeBox.getBox();
-		eb.setPrefWidth(WIDTH / 2);
-		cb.setPrefWidth(WIDTH / 2);
+		VBox eb = this.errorBox.getBox();
+		VBox cb = this.codeBox.getBox();
+		eb.prefWidthProperty().bind(root.widthProperty().divide(2));
+		cb.prefWidthProperty().bind(root.widthProperty().divide(2));
 		boxes.getChildren().addAll(eb, cb);
 		
 		// add the nodes to the root
@@ -59,29 +67,114 @@ public class GUI extends Application {
 		
 		// show stage
 		Scene scene = new Scene(root, WIDTH, HEIGHT);
+		
 		stage.setScene(scene);
 		stage.setTitle("Goodbye World");
+		stage.setFullScreen(true);
 		stage.show();
-    }
-
-    public void readControls(ListedLabelBox llb, String file, String editDelimiter) throws FileNotFoundException {
-    	Scanner scanner = new Scanner(new File(file));
-    	
-    	while (scanner.hasNextLine()) {
-    		String line = scanner.nextLine();
-			
-    		if (line.startsWith(editDelimiter)) {
-    			line = line.substring(editDelimiter.length());
-    			TextField field = new TextField(line);
-    			llb.add(field);
-			} else {
-    			Text text = new Text(line);
-    			llb.add(text);
-			}
-		}
 	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	private void readControls(ListedLabelBox llb, String file, String editDelimiter, boolean isConsole) throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File(file));
+		
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			
+			if (line.startsWith(editDelimiter)) {
+				line = line.substring(editDelimiter.length());
+				TextField field = new TextField(line);
+				llb.add(field);
+			} else {
+				if (isConsole) {
+					if (!line.startsWith("    ")) {
+						line = "> " + line;
+					} else {
+						line = "  " + line;
+					}
+				}
+				
+				Text text = new Text(line);
+				llb.add(text);
+			}
+		}
+		
+		scanner.close();
+	}
+	
+	private String readLine(String file) throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File(file));
+		String line = scanner.nextLine();
+		scanner.close();
+		return line;
+	}
+	
+	private List<String> readLines(String file) throws FileNotFoundException {
+		List<String> list = new ArrayList<>();
+		
+		Scanner scanner = new Scanner(new File(file));
+		
+		while (scanner.hasNextLine()) {
+			list.add(scanner.nextLine());
+		}
+		
+		scanner.close();
+		
+		return list;
+	}
+	
+	public void loadSet() throws FileNotFoundException {
+		if (this.index >= MAX_INDEX) {
+			return;
+		} else if (this.index == MAX_INDEX - 1) {
+			this.loadFinalSet();
+			return;
+		}
+		
+		this.errorBox.reset();
+		this.codeBox.reset();
+		
+		readControls(this.errorBox, ERR_TEMPLATE.replace("INDEX", String.valueOf(this.index)), EDIT_DELIMITER, true);
+		readControls(this.codeBox, BUG_TEMPLATE.replace("INDEX", String.valueOf(this.index)), EDIT_DELIMITER, false);
+		this.codeBox.setCorrectAnswer(readLine(FIX_TEMPLATE.replace("INDEX", String.valueOf(this.index))));
+		
+		String transitionText = this.index + 1 == 1 ? "_welcome operator_" : "_welcome back operator_";
+		
+		List<String> msgs = new ArrayList<>();
+		msgs.add(transitionText);
+		
+		try {
+			msgs.addAll(readLines(MSG_TEMPLATE.replace("INDEX", String.valueOf(this.index++))));
+		} catch (FileNotFoundException fnfe) {
+		}
+		
+		this.codeBox.addMsgs(msgs);
+	}
+	
+	public void loadFinalSet() throws FileNotFoundException {
+		this.codeBox.reset();
+		Parent p = this.codeBox.getBox().getParent();
+		((HBox) p).getChildren().remove(this.codeBox.getBox());
+		
+		this.errorBox.reset();
+		this.errorBox.add(new Text("Goodbye World"));
+		this.errorBox.getBox().setOnKeyTyped(e -> {
+			this.errorBox.getBox().setOnKeyTyped(e2 -> {});
+			
+			try {
+				List<String> msgs = readLines(MSG_TEMPLATE.replace("INDEX", "Final"));
+				this.errorBox.reset();
+				this.errorBox.getBox().setFillWidth(true);
+				
+				for (String m : msgs) {
+					this.errorBox.add(new Text(m));
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- src/goodbye/ListedLabelBox.java
+++ src/goodbye/ListedLabelBox.java
@@ -1,5 +1,10 @@
 package goodbye;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
@@ -10,9 +15,12 @@ import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ListedLabelBox {
@@ -27,43 +35,109 @@ public class ListedLabelBox {
 		}
 	}
 	
-	private List<Node> labels = new ArrayList<>();
+	private GUI gui;
+	private VBox box = new VBox();
+	private List<Node> children = box.getChildren();
+	private StringBuilder msg = new StringBuilder();
 	private SimpleObjectProperty<Paint> bgColorProperty;
 	private SimpleObjectProperty<Paint> textColorProperty;
 	private BackgroundFill bgFill;
+	private List<String> correctAnswers = new ArrayList<>();
 	
-	public ListedLabelBox() {
-		this(Color.BLACK, Color.WHITE);
+	public ListedLabelBox(GUI gui) {
+		this(gui, Color.BLACK, Color.WHITE);
 	}
 	
-	public ListedLabelBox(Color bgColor, Color textColor) {
-		bgColorProperty = new SimpleObjectProperty<>(bgColor);
-		textColorProperty = new SimpleObjectProperty<>(textColor);
-		bgFill = new BackgroundFill(bgColorProperty.get(), CornerRadii.EMPTY, Insets.EMPTY);
+	public ListedLabelBox(GUI gui, Color bgColor, Color textColor) {
+		this.gui = gui;
+		this.bgColorProperty = new SimpleObjectProperty<>(bgColor);
+		this.textColorProperty = new SimpleObjectProperty<>(textColor);
+		this.bgFill = new BackgroundFill(this.bgColorProperty.get(), CornerRadii.EMPTY, Insets.EMPTY);
+		
+		this.box.setBackground(new Background(this.bgFill));
+		this.box.setPadding(new Insets(10));
+	}
+	
+	public void addMsgs(List<String> msgs) {
+		this.msg = new StringBuilder();
+		msgs.forEach(m -> ListedLabelBox.this.msg.append(m).append('\n'));
 	}
 	
 	public void add(TextField field) {
-		this.labels.add(field);
-		String color = textColorProperty.get().toString();
-		color = color.substring(2);
-		field.setStyle("-fx-background-color: rgba(0, 0, 0, 0);" +
-				"-fx-text-fill: #f00;");
+		this.children.add(field);
+		String style = "-fx-background-color: rgba(0, 0, 0, 0);" +
+				"-fx-text-fill: #e30000;";
+		field.setStyle(style);
 		field.setFont(Font.font(displayFont.getFamily(), FontWeight.BOLD, FONT_SIZE));
+		field.selectedTextProperty().addListener(((observable, oldValue, newValue) -> field.deselect()));
+		field.textProperty().addListener(((observable, oldValue, newValue) -> {
+			for (String correctAnswer : ListedLabelBox.this.correctAnswers) {
+				if (equalsWithoutWS(correctAnswer, newValue)) {
+					field.setEditable(false);
+					field.setStyle("-fx-background-color: rgba(0, 0, 0, 0);" +
+							"-fx-text-fill: #009000");
+					Platform.runLater(() -> {
+						Text msgText = new Text(this.msg.toString());
+						msgText.setFont(displayFont);
+						
+						ListedLabelBox.this.children.clear();
+						gui.errorBox.children.clear();
+						gui.errorBox.add(msgText);
+						
+						gui.errorBox.box.requestFocus();
+						gui.errorBox.box.setOnKeyTyped(e -> {
+							try {
+								gui.errorBox.box.setOnKeyTyped(e2 -> {});
+								ListedLabelBox.this.gui.loadSet();
+							} catch (FileNotFoundException fnfe) {
+								fnfe.printStackTrace();
+								System.exit(1);
+							}
+						});
+					});
+				}
+			}
+		}));
+	}
+	
+	public void reset() {
+		this.children.clear();
 	}
 	
 	public void add(Text text) {
-		this.labels.add(text);
+		this.children.add(text);
 		text.setStroke(textColorProperty.get());
 		text.setFont(displayFont);
 	}
 	
+	public void setCorrectAnswer(String correctAnswer) {
+		this.correctAnswers.clear();
+		for (String ca : correctAnswer.split("~")) {
+			this.correctAnswers.add(ca);
+		}
+//		this.correctAnswers.addAll(Arrays.asList(correctAnswer.split("~")));
+	}
+	
 	public VBox getBox() {
-		VBox box = new VBox();
-		
-		box.getChildren().addAll(this.labels);
-		box.setBackground(new Background(bgFill));
-		box.setPadding(new Insets(10));
-		
 		return box;
+	}
+	
+	private static boolean equalsWithoutWS(String a, String b) {
+		StringBuilder at = new StringBuilder();
+		StringBuilder bt = new StringBuilder();
+		
+		for (int i = 0; i < a.length(); i++) {
+			if (a.charAt(i) != ' ' && a.charAt(i) != '\t') {
+				at.append(a.charAt(i));
+			}
+		}
+		
+		for (int i = 0; i < b.length(); i++) {
+			if (b.charAt(i) != ' ' && b.charAt(i) != '\t') {
+				bt.append(b.charAt(i));
+			}
+		}
+		
+		return at.toString().equals(bt.toString());
 	}
 }
